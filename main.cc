a1f624795e88178d71e4d7e7820e8871010b7cdd
jakedcolbert <jake.colbert@gordon.edu>
2022-01-18 13:13:46 -0500

    Initial start up questions configured - pair programmed

---
+++ main.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+/**
+    Main function
+ 
+    The function that executes the main program
+ */
+
+int main(){
+    int numOfOrganisms;
+    string locations;
+    int numOfGenerations;
+    
+    cout << "How many organisms initially? ";
+    cin >> numOfOrganisms;
+    
+    cout << "Locations? ";
+    cin >> locations;
+    
+    cout << "Generations? ";
+    cin >> numOfGenerations;
+    
+    while (cin.get() != '\n') { }  // see implementation notes #4
+    
+}
+
