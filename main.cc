ee0b3eee8e6de87302ef3ca5e7ab42c8a1059f51
jakedcolbert <jake.colbert@gordon.edu>
2022-01-20 15:41:15 -0500

    Added generate board function to main. The function takes a 2 dimensional array and outputs an animation to the console.

--- main.cc
+++ main.cc
@@ -3,6 +3,64 @@
 
 using namespace std;
 
+using namespace std;
+
+static const char ESC = 27; 
+
+static const bool frameByFrame = true;
+static const int boardWidth = 50;
+static const int boardHeight = 18;
+
+//two dimensional array that holds integers for now but should hold cell class objects
+int theBoard[boardWidth][boardHeight];
+
+
+/**
+    Generate board function
+ 
+    The function that builds a frame and generates a new board
+ 
+    Parameters:
+    int newBoard - a two dimensional array of cell objects that contain the new generation's organism positions
+ */
+
+void generateBoard(string newBoard){
+    
+    //top border
+    cout << "+";
+    for(int i = 0; i < boardWidth; i++){
+        cout << "-";
+    }
+    cout << "+\n";
+    
+    // left/right borders and organisms
+    
+    for(int row = 0; row < boardHeight; row++){
+        cout << "|";
+        for(int col = 0; col < boardWidth; col++){
+            cout << theBoard[row][col];
+        }
+        cout << "|\n";
+    }
+    
+    //bottom border
+    cout << "+";
+    for(int i = 0; i < boardWidth; i++){
+        cout << "-";
+    }
+    cout << "+\n";
+    
+    
+    //This code block allows the animation to be shown frame by frame
+    //pressing the return key shows the next frame
+    
+    if(frameByFrame){
+        cout << ESC << "[23;1H" << ESC << "[K"  << "Press RETURN to continue"; 
+        while (cin.get() != '\n') { };
+    }
+    
+}
+
 /**
     Main function
  
